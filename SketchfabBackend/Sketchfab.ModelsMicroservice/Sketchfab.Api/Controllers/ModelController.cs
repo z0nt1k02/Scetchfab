03abd56887743a99e23377af585d077f3ca292aa
Igor <hfhfhfhhjrj@gmail.com>
2025-10-01 14:25:36 +0500

    Add folder

---
+++ SketchfabBackend/Sketchfab.ModelsMicroservice/Sketchfab.Api/Controllers/ModelController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sketchfab.Application.Interfaces;
+using System.Threading.Tasks;
+
+
+
+namespace Sketchfab.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelController : ControllerBase
+    {
+
+        private readonly IModelService _modelService;
+
+        public ModelController(IModelService modelService)
+        {
+            _modelService = modelService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetModel(Guid id)
+        {
+            try
+            {
+                var (fileStream, filename, mimeType) = await _modelService.GetModel(id);
+                return File(fileStream, mimeType, filename);
+
+            }
+            catch (NullReferenceException ex)
+            {
+                return NotFound($"Файл с таким названием не найден: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("postModel")]
+        public async Task<IActionResult> PostModel([FromForm]IFormFile file)
+        {
+            const long maxFileSize = 20 * 1024 * 1024;
+            try
+            {
+                if (file.Length > maxFileSize)
+                {
+                    return BadRequest("Файл слишком большой");
+                }
+                if (file.Length == 0 || file == null)
+                {
+                    return BadRequest("Файл не должен быть пустым");
+                }
+                
+                var extension = Path.GetExtension(file.FileName).ToLowerInvariant();
+                if (extension != ".fbx")
+                {
+                    return BadRequest("Только fbx файлы");                   
+                }
+                using (Stream stream = file.OpenReadStream())
+                {
+                    await _modelService.PostModel(stream, file.FileName);
+                }
+
+                return Ok("Файл успешно загружен");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+
+        }
+
+        private string GetMimeType(string fileName)
+        {
+            var extension = Path.GetExtension(fileName).ToLowerInvariant();
+
+            return extension switch
+            {
+                ".glb" => "model/gltf-binary",
+                ".gltf" => "model/gltf+json",
+                ".fbx" => "application/octet-stream",
+                ".obj" => "model/obj",
+                ".stl" => "model/stl",
+                ".3ds" => "application/octet-stream",
+                _ => "application/octet-stream"
+            };
+        }
+    }
+}
---
+++ SketchfabBackend/Sketchfab.ModelsMicroservice/Sketchfab.Application/Services/ModelService.cs
@@ -0,0 +1,102 @@
+﻿using Sketchfab.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Sketchfab.Core.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace Sketchfab.Application.Services
+{
+    public class ModelService : IModelService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ISketchfabDbContext _context;
+        public ModelService(IConfiguration configuration, ISketchfabDbContext context   )
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+        public async Task<(Stream, string, string)> GetModel(Guid id)
+        {
+            try
+            {
+                var model = await _context.Models.FindAsync(id);
+                if(model is null)
+                {
+                    throw new NullReferenceException("Файл с таким id не найден");
+                }
+                string extension = Path.GetExtension(model.ModelName).ToLowerInvariant();
+                string path = Path.Combine(_configuration["FilesPaths:ModelsPath"]!, $"{model.Id}{extension}");
+                var fileStream = new FileStream(
+                    path,
+                    FileMode.Open,
+                    FileAccess.Read,
+                    FileShare.Read,
+                    bufferSize: 65536, 
+                    useAsync: true);
+
+                string mimeType = GetMimeType(model.ModelName);
+
+                
+                return (fileStream, model.ModelName, mimeType);
+            }catch(FileNotFoundException)
+            {
+                throw new FileNotFoundException($"Файл с таким именем не найден");
+            }
+                       
+        }
+
+        public async Task PostModel(IFormFile model, IFormFile modelImage,string title)
+        {            
+            Guid id = Guid.NewGuid();
+            string modelPath;
+            string modelImagePath;
+            var paths = _configuration.GetSection("FilesPaths");
+
+            using Stream stream = model.OpenReadStream();          
+            modelPath = await SaveFile(stream, paths["ModelsPath"]!,
+                $"{id.ToString()}{Path.GetExtension(model.FileName).ToLowerInvariant()}");
+
+            using Stream streamImage = modelImage.OpenReadStream();           
+            modelImagePath = await SaveFile(streamImage, paths["ModelsImagesPath"]!,
+                $"{id.ToString()}{Path.GetExtension(modelImage.FileName).ToLowerInvariant()}");
+                                  
+            var modelEntity = new ModelEntity
+            {
+               Id = id,
+               ModelName = model.FileName,
+               ImageName = modelImage.FileName,   
+               Title = title
+            };
+
+            _context.Models.Add(modelEntity);
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task<string> SaveFile(Stream file,string folderPath,string fileName)
+        {
+            string filePath = Path.Combine(folderPath, fileName);
+            
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return filePath;
+        }
+        
+       
+        private string GetMimeType(string fileName)
+        {
+            var extension = Path.GetExtension(fileName).ToLowerInvariant();
+
+            return extension switch
+            {
+                ".glb" => "model/gltf-binary",
+                ".gltf" => "model/gltf+json",
+                ".fbx" => "application/octet-stream",
+                ".obj" => "model/obj",
+                ".stl" => "model/stl",
+                ".3ds" => "application/octet-stream",
+                _ => "application/octet-stream"
+            };
+        }
+    }
+}
